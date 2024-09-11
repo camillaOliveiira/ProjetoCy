aedc7297089e3c46c7cf8ef1d03058346de520f3
Camila Oliveira <camila_oliveiira1@hotmail.com>
2024-09-11 18:01:25 -0300

    Update pageobject.js
    Update pageobject.js

--- cypress/support/Pages/pageobject.js
+++ cypress/support/Pages/pageobject.js
@@ -7,8 +7,8 @@ class LoginPage {
           return this;
         }
         enterusernamepassword(username, password) {
-            cy.get("#username").type("username");
-            cy.get("#password").type("password");
+          cy.get("#username").type("camila.oliveira");
+          cy.get("#password").type("Novasenha123");
           return this;
         }
         clickSignButton() {
@@ -20,6 +20,9 @@ class LoginPage {
             cy.get("#username").type("username");
             cy.get("#password").type("password");
           return this;
+        }
+        checkUser() {
+          cy.contains('#menuUserLink').type('camila.oliveira').check();
         }
  }
 
@@ -41,7 +44,94 @@ class LoginPage {
 
  }
 
+  class InfoProduct{
 
+    ProductSpeakers() {
+     cy.visit("https://advantageonlineshopping.com/#/category/Speakers/4");
+    }
+
+    TitleProduct() {
+      cy.contains('BOSE SOUNDLINK BLUETOOTH SPEAKER III');
+    }
+
+    PriceProduct() {
+      cy.contains('$269.99');
+    }
+
+    descricaoProduct() {
+      cy.contains('Meet the speaker that brings your music and friends together-with a style thats all your own. The SoundLink® Bluetooth speaker III is our best-performing mobile Bluetooth speaker. It plays louder and longer than its popular predecessor, with advanced Bose® technologies that reproduce the fullness, clarity and depth of your music. And a colorful assortment of optional covers let you personalize your speaker.');
+    }
+
+    ColorProduct() {
+      cy.contains('.Colors').should('exist');
+    }
+
+    ProductEscolhido() {
+      cy.get('[data-ng-show="([] | productsFilterForCategoriesProduct:searchResult:minPriceToFilter:maxPriceToFilter:productsInclude).length != 0"] > ul > :nth-child(1)').click();
+    }
+
+    ADDTOCART() {
+      cy.get('.fixedBtn > .roboto-medium').click();
+    }
+
+  }
+
+  class Carrinho{
+    popupCar() {
+      cy.get('tbody').should('exist');
+    }
+    ListaCar() {
+      cy.get('#shoppingCartLink').click();
+    }
+    PageCar() {
+     cy.visit("https://advantageonlineshopping.com/#/shoppingCart")
+    }
+    CarTitle() {
+      cy.contains('SHOPPING CART(1)').should('.sticky(1)').check();
+    }
+    CarProdName() {
+      cy.contains('').should('.fixedTableEdgeCompatibility > tbody > tr.ng-scope > :nth-child(2)').check();
+    }
+    CarProdColor() {
+      cy.contains('').should('tr.ng-scope > :nth-child(4)').check();
+    }
+    CarProdPrice() {
+      cy.contains('').should('tr.ng-scope > :nth-child(6)').check();
+    }
+  }
+
+  class Checkout {
+    BtnCheckout() {
+      cy.get('#checkOutButton').click();
+    }
+    OrderPayment() {
+      cy.contains('#userDetails').check();
+      cy.contains('#userCart').check();
+    }
+    BtnNext() {
+      cy.get('.mobileBtnHandler > #next_btn').click();
+    }
+    Payment() {
+      cy.get('.selected > input').click();
+      cy.get('#creditCard').type("5307461187785012");
+      cy.get('.creditCard > .inputContainer > .ng-pristine').type("628");
+      cy.get('[a-hint="MM"] > .inputContainer > .ng-pristine').click().type('07').click();
+      cy.get('[a-hint="YYYY"] > .inputContainer > .ng-pristine').click().type('2026').click();
+      cy.get('[a-hint="Cardholder name"] > .inputContainer > .ng-pristine').type('Camila Oliveira')
+      cy.get('[ng-show="(imgRadioButton == 2) && (noCards || showMasterCart)"] > .secForm > .option > .ng-pristine').click();
+    }
+    PayNow() {
+      cy.get('#pay_now_btn_ManualPayment').click();
+    }
+    Sucesso() {
+      cy.check('Product Added Successfully').should('exist');
+    }
+  }
  
  const login = new LoginPage();
- export default login;
+ const Buscar = new Buscar();
+ const InfoProduct = new InfoProduct();
+ const Carrinho = new Carrinho();
+ const Checkout = new Checkout();
+
+
