32ac20b175e7d7752099b9d41d14f74f82a7b774
Camila Oliveira <camila_oliveiira1@hotmail.com>
2024-09-11 17:56:15 -0300

    Update ValidarCarrinho.cy.js
    Update ValidarCarrinho.cy.js

--- cypress/e2e/ValidarCarrinho.cy.js
+++ cypress/e2e/ValidarCarrinho.cy.js
@@ -1,16 +1,29 @@
-describe('Validar Produto no Carrinho', () => {
-    it(' Dado que acesso o site Advantage', () => {
-        cy.visit('https://advantageonlineshopping.com/#/')
-    })
-    it('Quando busco por mice e pressiono Enter\n' +'E clico no produto mice escolhido\n' +
-    'E clico em ADD TO CART\n' + 'E clico no carrinho de compras', () => {
-    })
-    it('Entao devo visualizar pagina do carrinho com os itens inclusos', () => {
-        cy.visit('https://advantageonlineshopping.com/#/')
-        cy.get('#mobileSearch > .roboto-medium').type('mice').type('{enter}')
-        cy.get('[data-ng-show="([] | productsFilterForCategoriesProduct:searchResult:minPriceToFilter:maxPriceToFilter:productsInclude).length != 0"] > ul > :nth-child(3)').click()
-        cy.get('.fixedBtn > .roboto-medium').click()
-        cy.visit('https://advantageonlineshopping.com/#/product/28?viewAll=mice')
-        cy.get('[data-ng-mouseenter="enterCart()"]').click()
-    })
+import {Given, And, When, Then, cucumberTemplate} from "cypress-cucumber-preprocessor/lib/cucumberTemplate";
+import {ELEMENTS} from "../support/Pages/Elements";
+import {pageobject} from "../support/Pages/pageobject";
+import { features } from "process";
+import login from "../../support/Pages/pageobject";
+import(cucumberTemplate);
+import(cucumberJson);
+import(ELEMENTS);
+import(features);
+import(pageobject);
+
+
+Given('que eu tenha produtos inclusos no carrinho', () => {
+    login.enterURL();
+    return true;
+});
+
+When('acessar a tela do carrinho', () => {
+    cy.contains('SHOPPING CART').should('#shoppingCartLink(1)').check();
+    Carrinho.ListaCar();
+    return true;
 })
+
+Then('devo visualizar informações dos itens inclusos', () => {
+    Carrinho.CarTitle();
+    Carrinho.CarProdName();
+    Carrinho.CarProdColor();
+    Carrinho.CarProdPrice();
+    })
