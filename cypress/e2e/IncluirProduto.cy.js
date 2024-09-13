6382be294547b88768f763d8399720fe27aeb9d7
Camila Oliveira <camila_oliveiira1@hotmail.com>
2024-09-13 16:46:17 -0300

    Delete cypress directory

--- cypress/e2e/IncluirProduto.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Incluir Produto no Carrinho', () => {
-    it('passes', () => {
-        cy.visit('https://advantageonlineshopping.com/#/')
-        cy.get('#mobileSearch > .roboto-medium').type('mice')
-            .type('{enter}')
-        cy.get('[data-ng-show="([] | productsFilterForCategoriesProduct:searchResult:minPriceToFilter:maxPriceToFilter:productsInclude).length != 0"] > ul > :nth-child(3)').click()
-        cy.get('.fixedBtn > .roboto-medium').click()
-    })
-})
--- cypress/support/Pages/Elements.js
+++
@@ -1,7 +0,0 @@
-export const ELEMENTS = {
-    username: '[a-hint="Username"]',
-    password: '[a-hint="Password"]',
-    loginButton: '[#sign_in_btn]',
-    LoginPage: '[https://advantageonlineshopping.com/#/]',
-    
-}
