f21627e74a00353462a3394b7a10a9ce1a026aba
Camila Oliveira <camila_oliveiira1@hotmail.com>
2024-09-13 09:39:19 -0300

    Update cypress.config.js
    Atualizando

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,24 @@
+
 const { defineConfig } = require("cypress");
-const cucumber = require("cypress-cucumber-preprocessor").default;
+const cucumber = require('cypress-cucumber-preprocessor').default;
+
 module.exports = defineConfig({
- e2e: {
-   specPattern: "**/*.feature",
-   setupNodeEvents(on, config) {
-     on("file:preprocessor", cucumber());
-   },
- },
+  
+  e2e: {
+    projectId: 'yar8bd',
+    setupNodeEvents(on, _config) {
+      on('file:preprocessor', cucumber());     
+    },
+  
+    specPattern: "cypress/e2e/step_definitions/login/*.feature",
+  },
+/*video: true ,
+videoCompression: false,
+videosFolder: 'cypress/videos',
+viewportHeight: 660,
+viewportWidth: 1000,
+waitForAnimations: true,
+watchForFileChanges: true,
+appid: "<apiKey>",**/
+
 });
