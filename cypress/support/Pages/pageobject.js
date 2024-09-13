b8d70df2567b1fac13eb45f603e92f07ae6acf3b
Camila Oliveira <camila_oliveiira1@hotmail.com>
2024-09-13 09:40:53 -0300

    Update pageobject.js
    Atualizando

--- cypress/support/Pages/pageobject.js
+++ cypress/support/Pages/pageobject.js
@@ -1,32 +1,30 @@
+const login = new LoginPage()
+const URL = 'https://advantageonlineshopping.com/#/'
+const SIGN = '#menuUser'
+const USER_INPUT = 'input[name="username"]'
+const PASS_INPUT = 'input[name="password"]'
+const SUBMIT_BUTTON = '#sign_in_btn'
+
+
 class LoginPage {
-        enterURL() {
-          cy.visit("https://advantageonlineshopping.com/#/");
+        static enterURL() {
+          cy.visit(URL)
         }
-        clickSignInLink() {
-            cy.get('#menuUserLink').click();
-          return this;
+        static clickSignInLink() {
+            cy.get(SIGN).click()
         }
-        enterusernamepassword(username, password) {
-          cy.get("#username").type("camila.oliveira");
-          cy.get("#password").type("Novasenha123");
-          return this;
+        static enterusername(username) {
+          cy.get(USER_INPUT).type('username')
         }
-        clickSignButton() {
-            cy.get('#loginButton').click().click();
-          return this;
+        static enterpassword(password){
+          cy.get(PASS_INPUT).type('Password1')
         }
-        
-        enteruserpassinvalid(usernamein, passwordin) {
-            cy.get("#username").type("username");
-            cy.get("#password").type("password");
-          return this;
-        }
-        checkUser() {
-          cy.contains('#menuUserLink').type('camila.oliveira').check();
+        static clickSignButton() {
+            cy.get(SUBMIT_BUTTON).click()
         }
  }
 
- class Buscar{
+ /*class Buscar{
 
   barradepesquisa() {
     cy.get('#mobileSearch > .roboto-medium').click()
@@ -47,91 +45,86 @@ class LoginPage {
   class InfoProduct{
 
     ProductSpeakers() {
-     cy.visit("https://advantageonlineshopping.com/#/category/Speakers/4");
+     cy.visit("https://advantageonlineshopping.com/#/category/Speakers/4")
     }
 
     TitleProduct() {
-      cy.contains('BOSE SOUNDLINK BLUETOOTH SPEAKER III');
+      cy.contains('BOSE SOUNDLINK BLUETOOTH SPEAKER III')
     }
 
     PriceProduct() {
-      cy.contains('$269.99');
+      cy.contains('$269.99')
     }
 
     descricaoProduct() {
-      cy.contains('Meet the speaker that brings your music and friends together-with a style thats all your own. The SoundLink® Bluetooth speaker III is our best-performing mobile Bluetooth speaker. It plays louder and longer than its popular predecessor, with advanced Bose® technologies that reproduce the fullness, clarity and depth of your music. And a colorful assortment of optional covers let you personalize your speaker.');
+      cy.contains('Meet the speaker that brings your music and friends together-with a style thats all your own. The SoundLink® Bluetooth speaker III is our best-performing mobile Bluetooth speaker. It plays louder and longer than its popular predecessor, with advanced Bose® technologies that reproduce the fullness, clarity and depth of your music. And a colorful assortment of optional covers let you personalize your speaker.')
     }
 
     ColorProduct() {
-      cy.contains('.Colors').should('exist');
+      cy.contains('.Colors').should('exist')
     }
 
     ProductEscolhido() {
-      cy.get('[data-ng-show="([] | productsFilterForCategoriesProduct:searchResult:minPriceToFilter:maxPriceToFilter:productsInclude).length != 0"] > ul > :nth-child(1)').click();
+      cy.get('[data-ng-show="([] | productsFilterForCategoriesProduct:searchResult:minPriceToFilter:maxPriceToFilter:productsInclude).length != 0"] > ul > :nth-child(1)').click()
     }
 
     ADDTOCART() {
-      cy.get('.fixedBtn > .roboto-medium').click();
+      cy.get('.fixedBtn > .roboto-medium').click()
     }
 
   }
 
   class Carrinho{
     popupCar() {
-      cy.get('tbody').should('exist');
+      cy.get('tbody').should('exist')
     }
     ListaCar() {
-      cy.get('#shoppingCartLink').click();
+      cy.get('#shoppingCartLink').click()
     }
     PageCar() {
      cy.visit("https://advantageonlineshopping.com/#/shoppingCart")
     }
     CarTitle() {
-      cy.contains('SHOPPING CART(1)').should('.sticky(1)').check();
+      cy.contains('SHOPPING CART(1)').should('.sticky(1)').check()
     }
     CarProdName() {
-      cy.contains('').should('.fixedTableEdgeCompatibility > tbody > tr.ng-scope > :nth-child(2)').check();
+      cy.contains('').should('.fixedTableEdgeCompatibility > tbody > tr.ng-scope > :nth-child(2)').check()
     }
     CarProdColor() {
-      cy.contains('').should('tr.ng-scope > :nth-child(4)').check();
+      cy.contains('').should('tr.ng-scope > :nth-child(4)').check()
     }
     CarProdPrice() {
-      cy.contains('').should('tr.ng-scope > :nth-child(6)').check();
+      cy.contains('').should('tr.ng-scope > :nth-child(6)').check()
     }
   }
 
   class Checkout {
     BtnCheckout() {
-      cy.get('#checkOutButton').click();
+      cy.get('#checkOutButton').click()
     }
     OrderPayment() {
-      cy.contains('#userDetails').check();
-      cy.contains('#userCart').check();
+      cy.contains('#userDetails').check()
+      cy.contains('#userCart').check()
     }
     BtnNext() {
-      cy.get('.mobileBtnHandler > #next_btn').click();
+      cy.get('.mobileBtnHandler > #next_btn').click()
     }
     Payment() {
-      cy.get('.selected > input').click();
-      cy.get('#creditCard').type("5307461187785012");
-      cy.get('.creditCard > .inputContainer > .ng-pristine').type("628");
-      cy.get('[a-hint="MM"] > .inputContainer > .ng-pristine').click().type('07').click();
-      cy.get('[a-hint="YYYY"] > .inputContainer > .ng-pristine').click().type('2026').click();
+      cy.get('.selected > input').click()
+      cy.get('#creditCard').type('5307461187785012')
+      cy.get('.creditCard > .inputContainer > .ng-pristine').type('628')
+      cy.get('[a-hint="MM"] > .inputContainer > .ng-pristine').click().type('07').click()
+      cy.get('[a-hint="YYYY"] > .inputContainer > .ng-pristine').click().type('2026').click()
       cy.get('[a-hint="Cardholder name"] > .inputContainer > .ng-pristine').type('Camila Oliveira')
-      cy.get('[ng-show="(imgRadioButton == 2) && (noCards || showMasterCart)"] > .secForm > .option > .ng-pristine').click();
+      cy.get('[ng-show="(imgRadioButton == 2) && (noCards || showMasterCart)"] > .secForm > .option > .ng-pristine').click()
     }
     PayNow() {
-      cy.get('#pay_now_btn_ManualPayment').click();
+      cy.get('#pay_now_btn_ManualPayment').click()
     }
     Sucesso() {
-      cy.check('Product Added Successfully').should('exist');
+      cy.check('Product Added Successfully').should('exist')
     }
-  }
+  }**/
  
- const login = new LoginPage();
- const Buscar = new Buscar();
- const InfoProduct = new InfoProduct();
- const Carrinho = new Carrinho();
- const Checkout = new Checkout();
-
-
+export default LoginPage
+ 
