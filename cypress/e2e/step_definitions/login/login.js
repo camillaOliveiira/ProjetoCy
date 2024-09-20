49d14e84300ada89ccc99d6d3285430001bcefd2
Camila Oliveira <camila_oliveiira1@hotmail.com>
2024-09-20 12:13:54 -0300

    Modificado
    Modificado

--- cypress/e2e/step_definitions/login/login.js
+++ cypress/e2e/step_definitions/login/login.js
@@ -1,13 +1,17 @@
 import {Given, When, Then} from 'cypress-cucumber-preprocessor/steps'
+import LoginPage from '../../../support/PageObjects/VisitSit';
 
 Given("I'm on the login page", () =>{
-    cy.visit('https://advantageonlineshopping.com/#/');
+    LoginPage.enterURL();
+    LoginPage.clickSignInLink();
 })
 
-When("I type a registered my email and password", () =>{
-
+When("I type a registered my username and password", () =>{
+    LoginPage.enterusername();
+    LoginPage.enterpassword();
+    LoginPage.clickSignButton();
 })
 
 Then("I have a susseccful login", () =>{
-    
+    cy.get('#menuUserLink').should('be.visible');
 })
